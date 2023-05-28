b47f4a5857db7021831810cbf7dc39f6d9e5c438
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-05-28 12:21:10 +0100

    Implemented collection view

--- piggy-bank/ViewControllers/Home/BankCardCollectionViewCell.swift
+++ piggy-bank/ViewControllers/Home/BankCardCollectionViewCell.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class BankCardCollectionViewCell: UICollectionViewCell {
-
+    
+    var card: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        card = UIView()
+        card.translatesAutoresizingMaskIntoConstraints = false
+        card.backgroundColor = .systemOrange
+        contentView.addSubview(card)
+        
+        NSLayoutConstraint.activate([
+            card.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            card.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            card.widthAnchor.constraint(equalToConstant: 300),
+            card.heightAnchor.constraint(equalToConstant: 200),
+        ])
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
 }
--- piggy-bank/ViewControllers/Home/CardTableViewCell.swift
+++ piggy-bank/ViewControllers/Home/CardTableViewCell.swift
@@ -6,18 +6,45 @@
 //
 
 import UIKit
+import CollectionViewPagingLayout
 
-class CardTableViewCell: UITableViewCell {
+class CardTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    private let layout = CollectionViewPagingLayout()
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        configureCollectionView()
+    }
+
+    private func configureCollectionView() {
+        collectionView.register(BankCardCollectionViewCell.self, forCellWithReuseIdentifier: "BankCardCell")
+        collectionView.isPagingEnabled = true
+        collectionView.dataSource = self
+        layout.numberOfVisibleItems = 7
+        layout.scrollDirection = .horizontal
+        layout.transparentAttributeWhenCellNotLoaded = true
+        collectionView.collectionViewLayout = layout
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.clipsToBounds = false
+        collectionView.backgroundColor = .clear
+        collectionView.scrollsToTop = false
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BankCardCell", for: indexPath) as! BankCardCollectionViewCell
+        return cell
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-
     }
-    
 }
+
+
